3e2420da92896e355b33ff9f56a2614b6ed90488
Scott Bilas <scottbilas@gmail.com>
2018-08-20 17:08:37 +0200

    couple more exercises

--- ch1.rkt
+++ ch1.rkt
@@ -2,6 +2,7 @@
 #lang racket
 
 (require sicp)
+(require racket/trace)
 (require rackunit rackunit/text-ui)
 
 (define ch1-tests (test-suite "ch1"
@@ -90,6 +91,69 @@
 
 (check-eq? 7 (a-plus-abs-b 4 3))
 (check-eq? 7 (a-plus-abs-b 4 -3))
+
+) ;case
+
+;;;
+(test-case "ch1-1.7"
+;;;
+
+(define (sqrt-iter guess last x epsilon)
+  (if (good-enough? guess last epsilon)
+      guess
+      (sqrt-iter (improve guess x) guess x epsilon)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess last epsilon)
+;  (< (abs (- (* guess guess) x)) epsilon))
+  (< (abs (- last guess)) (* epsilon guess)))
+
+(define (sqrt x epsilon)
+  (sqrt-iter 1.0 (+ 1.0 (* 2.0 epsilon)) x epsilon))
+
+(check-= 3 (sqrt 9 .001) .001)
+(check-= 11.705 (sqrt (+ 100 37) .001) .001)
+(check-= 1.774 (sqrt (+ (sqrt 2 .001) (sqrt 3 .001)) .001) .001)
+
+(check-= 31622.777 (sqrt 1000000000 .0001) .001)
+(check-= 1000000 (sqrt 1000000000000 .0001) .001)
+(check-= .01 (sqrt .0001 .000001) .00001)
+
+) ;case
+
+;;;
+(test-case "ch1-1.8"
+;;;
+
+(define (curt-iter guess last x tolerance)
+  (if (good-enough? guess last tolerance)
+      guess
+      (curt-iter (improve guess x) guess x tolerance)))
+
+(define (improve guess x)
+  (average
+    guess
+    (/ (+ (/ x (* guess guess)) (* 2 guess)) 3)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess last tolerance)
+  (< (abs (- last guess)) (* tolerance guess)))
+
+(define (curt x tolerance)
+  (curt-iter 1.0 (+ 1.0 (* 2.0 tolerance)) x tolerance))
+
+(check-= 3 (curt 27 .000001) .001)
+(check-= 4.641 (curt 100 .000001) .001)
+(check-= 1000 (curt 1000000000 .000001) .001)
+(check-= 10000 (curt 1000000000000 .00000001) .001)
+(check-= .04641 (curt .0001 .0000001) .00001)
 
 ) ;case
 
